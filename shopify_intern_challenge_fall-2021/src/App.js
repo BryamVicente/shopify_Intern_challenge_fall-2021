cb9a849a2221caccfc42fffc8038dc9cb018c200
github username <vicenteBryam@gmail.com>
2021-05-03 20:32:09 -0400

    started

--- shopify_intern_challenge_fall-2021/src/App.js
+++ shopify_intern_challenge_fall-2021/src/App.js
@@ -2,23 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+// Notes
+  // This app is strictly about being a search engine for Movies
+
+  // I'm using an API to search the movies 
+    // figure out how to use the search feature to go through the api
+    // I Think I can create a seprate component for the search feature 
+    // search feature needs to filter through the movies --> check instructions just in case 
+  //
+
+  // Creat a separate component for the favorite's list 
+    // for this feature I only need both GET and DELETE requests 
+    // 
+  // 
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  
+    </>
   );
 }
 
